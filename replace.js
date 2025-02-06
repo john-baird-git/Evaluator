288210daf7b7041c5f74d4c984286998ac8e118f
john-baird-git <john.w.baird@btconnect.com>
2025-02-06 21:45:35 +0000

    10

--- replace.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const apiKey = process.env.API_KEY;
-const content = fs.readFileSync('index.html', 'utf8');
-const replaced = content.replace('ANTHROPIC_API_KEY_PLACEHOLDER', apiKey);
-fs.writeFileSync('index.html', replaced);
